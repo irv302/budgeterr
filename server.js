c3bc13724189ef490e707960c7a4f7388f8521c6
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-05-23 05:08:33 -0500

    keep going

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const req = require('express/lib/request');
-const app = express();
-const port = 3000;
-const budget = require('./modles/budget');
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+// const budget = require('./models/budget')
+const bodyParser = require('body-parser')
 
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: false}))
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+})
 
 app.get('/budgets', (req, res) => {
     res.render('index.ejs', {budgetAll:budget})
@@ -15,7 +17,7 @@ app.get('/budgets/new', (req, res) => {
     res.render('new.ejs')
 })
 app.get('/budgets', (req, res) => {
-    budget.push(req.body);
+    budget.push(req.body)
     console.log(req.body)
     res.redirect('/budgets')
     
@@ -24,7 +26,7 @@ app.get('/budgets/:index', (req, res) => {
     res.render('show.ejs', {budgetAll:budget [req.params.index]})
 })
 
-app.listen(port, () => {
+app.listen(3000, () => {
     console.log('I am listening to, 3000')
 })
 
