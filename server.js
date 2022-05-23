3358aec30fb4ffe1cdffe9b2c3ba6b307d198136
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-05-23 03:12:27 -0500

    keep working

--- server.js
+++ server.js
@@ -4,5 +4,23 @@ const port = 3000;
 
 
 
+app.get('/budgets', (req, res) => {
+    res.render('index.ejs', {budgetAll:budget})
+})
+app.get('/budgets/new', (req, res) => {
+    res.render('new.ejs')
+})
+app.get('/budgets', (req, res) => {
+    budget.push(req.body);
+    console.log(req.body)
+    res.redirect('/budgets')
+    
+})
+app.get('/budgets/:index', (req, res) => {
+    res.render('index.ejs', {budgetAll:budget [req.params.index]})
+})
 
+app.listen(port, () => {
+    console.log('I am listening to, 3000')
+})
 
